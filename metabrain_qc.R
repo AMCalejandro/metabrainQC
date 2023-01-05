fa67a0b95995d932eaefd2e4f1fcb887643faa1d
AMCalejandro <alejandro.carrasco.20@ucl.ac.uk>
2023-01-05 17:51:13 +0000

    Adding extra QC steps and do some housekeeping

--- metabrain_qc.R
+++ metabrain_qc.R
@@ -11,39 +11,47 @@ library(data.table)
 library(DT)
 
 
+# Create util to get the total sample size for each SNP
+sumSampleSize = function(string) {
+  unlist(regmatches(string, gregexpr("[[:digit:]]+", string))) %>%
+    as.numeric %>% 
+    sum
+}
 
 processing_function = function(tissue_chrfiles, 
                                tissue_name,
-                               .raw_data_path = raw_data_path,
-                               .qc_data_path = qc_data_path) {
+                               raw_data_path = rawpath,
+                               qc_data_path = qcpath) {
   
   # MAF file processing
   maf_index = grep(pattern="MAF", x = tissue_chrfiles)
-  maf = fread(paste(.raw_data_path, tissue_name, tissue_chrfiles[maf_index], sep = "/" ))
+  maf = fread(paste(raw_data_path, tissue_name, tissue_chrfiles[maf_index], sep = "/" ))
   tissue_chrfiles = tissue_chrfiles[-maf_index]
   
   # map each tissue chunk to the function that does the actual processing
   map(tissue_chrfiles, function(x,
                                 maf. = maf,
-                                tissue_name. = tissue_name, 
-                                raw_path = .raw_data_path,
-                                qc_path = .qc_data_path) {
+                                tissuename = tissue_name, 
+                                raw_path = raw_data_path,
+                                qc_path = qc_data_path) {
     
     tissuechr_name = x
     cat("\nProcessing", x, "\n")
-    tissuechr_chunk = fread(paste(raw_path, tissue_name., tissuechr_name, sep = "/" ))
+    tissuechr_chunk = fread(paste(raw_path, tissuename, tissuechr_name, sep = "/" ))
     cat("Dims before any QC", dim(tissuechr_chunk), "\n")
   
-    # Inner joining the maf file to the tissuechr_chunk
+    # Inner joining the maf file to the tissuechr_chunk and trim the ENDEMBL ID
     tissuechr_chunk_proc = tissuechr_chunk %>%
-      inner_join(maf. %>% dplyr::select(SNP, MAF), by = c("SNPName" = "SNP" ) ) %>%
-      mutate(ProbeName = gsub("\\..*", "", ProbeName))
+      dplyr::inner_join(maf. %>% dplyr::select(SNP, MAF), by = c("SNPName" = "SNP" ) ) %>%
+      dplyr::mutate(ProbeName = gsub("\\..*", "", ProbeName))
     
-    
-    # Another keyQC is to get the number of samples per SNP
-    sumSampleSize = function(string) {
-      unlist(regmatches(string, gregexpr("[[:digit:]]+", string))) %>% as.numeric %>% sum
-    }
+    # Get rsID on a column and CHR:BP on another column
+    snpname = as.data.frame(
+      stringr::str_split_fixed(
+        metabrain$SNPName, n = 4, pattern = ":"
+      ))
+    metabrain$SNP = snpname$V3
+    metabrain$CHRPOS = paste(snpname$V1, snpname$V2, sep = ":")
     
     # Find the column number with the sample size and process it
     sampleSize_index = grep("DatasetsNrSamples", colnames(tissuechr_chunk_proc))
@@ -51,36 +59,30 @@ processing_function = function(tissue_chrfiles,
     tissuechr_chunk_proc = cbind(tissuechr_chunk_proc, NrSamples)
     
     # Add column stating eQTL
-    tissuechr_chunk_proc$eQTL = paste0("eQTL_", tissue_name.)
+    tissuechr_chunk_proc$eQTL = paste0("eQTL_", tissuename)
     cat("Dims after QC was applied", dim(tissuechr_chunk_proc), "\n")
     
     # Write results in the QC path
     fwrite(tissuechr_chunk_proc, 
-           paste(qc_path, tissue_name., paste0("QCed_",tissuechr_name), sep = "/" ),
+           paste(qc_path, tissuename, paste0("QCed_",tissuechr_name), sep = "/" ),
            quote = F, sep = "\t", row.names=F, col.names=T)
-  }
-    )
-
+  })
 }
 
 
 setwd("/mnt/rreal/RDS/DATA/eQTLdata/METABRAIN")
-raw_data_path = "/mnt/rreal/RDS/DATA/eQTLdata/METABRAIN/RAW_DATA/"
-qc_data_path = "/mnt/rreal/RDS/DATA/eQTLdata/METABRAIN/METABRAIN_QC/"
+rawpath = "/mnt/rreal/RDS/DATA/eQTLdata/METABRAIN/RAW_DATA/"
+qcpath = "/mnt/rreal/RDS/DATA/eQTLdata/METABRAIN/METABRAIN_QC_V2/"
 
 # Get all tissues
-tissues = list.files(raw_data_path)
+tissues = list.files(rawpath)
 
 # Get all data for ea
 tissues_chromosomes =  map(tissues, function(tissue) {
-  allfiles = list.files(paste(raw_data_path, tissue, sep = "/"))
+  allfiles = list.files(paste(rawpath, tissue, sep = "/"))
   allfiles
 }) %>%
   setNames(tissues)
 
 map2(tissues_chromosomes, names(tissues_chromosomes), processing_function)
-
-
-
-
 
